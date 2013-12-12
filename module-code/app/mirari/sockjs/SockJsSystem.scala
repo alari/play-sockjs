99939e055043d73569387ab38009314ab370a012
Dmitry Kurinskiy <name.alari@gmail.com>
2013-12-12 15:08:35 +0400

    error reporting in initService

--- module-code/app/mirari/sockjs/SockJsSystem.scala
+++ module-code/app/mirari/sockjs/SockJsSystem.scala
@@ -4,6 +4,7 @@ import akka.actor.{ActorRef, Props, ActorSystem}
 import play.api.libs.json.Json
 import scala.util.Random
 import mirari.sockjs.service.SockJsService
+import play.api.Mode
 
 /**
  * @author alari
@@ -17,6 +18,8 @@ object SockJsSystem {
   def initService(name: String, handler: Props, websocket: Boolean = true, cookieNeeded: Boolean = true) {
     if(!services.contains(name))
       services += name -> system.actorOf(Props(new SockJsService(handler, websocket, cookieNeeded)), name)
+    else if(play.api.Play.current.mode != Mode.Test)
+      play.api.Logger.error("Trying to init service `" + name + "` but it has already been initiated!")
   }
 
   def service(name: String) = services.get(name)
